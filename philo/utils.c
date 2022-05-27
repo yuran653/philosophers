86488279d52bfe39d00d0fa4f7f936d1a9fe285f
Jerica Goldstein <jgoldste@pr-m1.kzn.21-school.ru>
2022-05-28 00:32:34 +0300

    print mutex have been fixed

--- philo/utils.c
+++ philo/utils.c
@@ -6,7 +6,7 @@
 /*   By: jgoldste <jgoldste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 22:24:43 by jgoldste          #+#    #+#             */
-/*   Updated: 2022/05/28 00:00:42 by jgoldste         ###   ########.fr       */
+/*   Updated: 2022/05/28 00:27:40 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,17 @@ int	mutex_unlock_return_2(t_philo *philo)
 
 int	print_status(t_philo *philo, t_params *params, char *action)
 {
+	pthread_mutex_lock(&params->print->mut);
 	pthread_mutex_lock(&params->death->mut);
 	if (!params->philo_is_dead)
 	{
 		pthread_mutex_unlock(&params->death->mut);
-		pthread_mutex_lock(&params->death->mut);
-		if (params->philo_is_dead)
-		{
-			pthread_mutex_unlock(&params->death->mut);
-			return (1);
-		}
-		pthread_mutex_unlock(&params->death->mut);
-		pthread_mutex_lock(&params->print->mut);
 		printf("\t[%7lldms] philosopher [%3d] %s\n",
 			get_timestamp() - philo->start, philo->id, action);
 		pthread_mutex_unlock(&params->print->mut);
 		return (0);
 	}
+	pthread_mutex_unlock(&params->print->mut);
 	pthread_mutex_unlock(&params->death->mut);
 	return (1);
 }
